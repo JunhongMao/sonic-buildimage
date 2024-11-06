b84380ed442153bac71fa864a6d4f2c93ff8de08
Junhong Mao <junhong.mao@nokia.com>
2024-11-06 11:42:46 -0500

    [Nokia-IXR7220] Add support for Nokia-IXR7220-H5-64O platform

--- platform/broadcom/one-image.mk
+++ platform/broadcom/one-image.mk
@@ -88,6 +88,7 @@ $(SONIC_ONE_IMAGE)_LAZY_INSTALLS += $(DELL_S6000_PLATFORM_MODULE) \
                                $(NOKIA_IXR7220H4_32D_PLATFORM_MODULE) \
                                $(NOKIA_IXR7220H4_64D_PLATFORM_MODULE) \
                                $(NOKIA_IXR7220H5_64D_PLATFORM_MODULE) \
+                               $(NOKIA_IXR7220H5_64O_PLATFORM_MODULE) \
                                $(TENCENT_TCS8400_PLATFORM_MODULE) \
                                $(TENCENT_TCS9400_PLATFORM_MODULE) \
                                $(UFISPACE_S6301_56ST_PLATFORM_MODULE) \
